3aae75760b7de7a6f5fb0cdc6926e5b08bda27c6
kevinkipkorir <kevinyator10@gmail.com>
2021-08-31 14:47:13 +0300

    Updates

---
+++ precache-manifest.a1013b5a750418b279117dc8f2811d07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ee23516c65735c0aafb29f53f101df1",
+    "url": "/ChuckNorris-API/index.html"
+  },
+  {
+    "revision": "d381b589d25c5d2583c6",
+    "url": "/ChuckNorris-API/static/css/2.79fd74fd.chunk.css"
+  },
+  {
+    "revision": "17d7058635a1518a926a",
+    "url": "/ChuckNorris-API/static/css/main.7c28b25b.chunk.css"
+  },
+  {
+    "revision": "d381b589d25c5d2583c6",
+    "url": "/ChuckNorris-API/static/js/2.d6c27a3f.chunk.js"
+  },
+  {
+    "revision": "17d7058635a1518a926a",
+    "url": "/ChuckNorris-API/static/js/main.84b9d195.chunk.js"
+  },
+  {
+    "revision": "75ad3644e4d9acc93132",
+    "url": "/ChuckNorris-API/static/js/runtime~main.8d6dd2b2.js"
+  },
+  {
+    "revision": "4c845d86fa88761a6535c8639ef4bb51",
+    "url": "/ChuckNorris-API/static/media/chuck.4c845d86.png"
+  }
+]);
